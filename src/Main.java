24491b4cdb2ca8e46ae088e09e2cec984bf909f8
student <student@vlab.local>
2023-11-10 16:09:22 +0100

    event loop and counter in java

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,39 @@
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int[] array = {10, 9, 8, 7, 6, 5, 4, 3, 2, 1};
+
+        int counter = 0;
+        System.out.println("Before: ");
+        System.out.println(Arrays.toString(array));
+
+
+        /******************TAsk1A*****************************/
+//        for (int j = 0; j < array.length - 1; j++) {
+//
+//           if (array[j] > array[j + 1]) {
+//               int greater = array[j];
+//               array[j] = array[j + 1];
+//                array[j + 1] = greater;
+//           }
+//
+//        }
+        for (int i = 0; i < array.length - 1; i++) {
+            counter++;
+            for (int j = 0; j < array.length - 1; j++) {
+                counter++;
+                if (array[j] > array[j + 1]) {
+                    counter++;
+                    int greater = array[j];
+                    array[j] = array[j + 1];
+                    array[j + 1] = greater;
+                }
+            }
+            System.out.println("After: ");
+            System.out.println(Arrays.toString(array));
+            System.out.println("num of actions: " + counter);
+
+        }
     }
 }
